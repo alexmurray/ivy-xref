86d2e425541f6b3d4a06439388d187924b181641
Alex Murray <murray.alex@gmail.com>
2017-12-01 14:37:45 +0900

    Initial version of ivy-xref

---
+++ README.md
@@ -0,0 +1,36 @@
+# ivy-xref - select from xref candidates with ivy
+
+[![License GPL 3](https://img.shields.io/badge/license-GPL_3-green.svg)](http://www.gnu.org/licenses/gpl-3.0.txt)
+[![MELPA](http://melpa.org/packages/ivy-xref-badge.svg)](http://melpa.org/#/ivy-xref)
+[![Build Status](https://travis-ci.org/alexmurray/ivy-xref.svg?branch=master)](https://travis-ci.org/alexmurray/ivy-xref)
+
+Use [Ivy](https://github.com/abo-abo/swiper) as the interface to select from
+xref candidates.
+
+![](https://raw.githubusercontent.com/alexmurray/ivy-xref/master/ivy-xref.png)
+
+## Installation
+
+### MELPA
+
+The preferred way to install `ivy-xref` is via
+[MELPA](http://melpa.org) - then you can just <kbd>M-x package-install RET
+ivy-xref RET</kbd>
+
+To enable then simply add the following to your init file:
+
+```emacs-lisp
+(require 'ivy-xref)
+(setq xref-show-xrefs-function #'ivy-xref-show-xrefs)
+```
+
+### Manual installation
+
+If you would like to install the package manually, download or clone it and
+place within Emacs' `load-path`, then enable as above.
+
+## License
+
+Copyright © 2017 Alex Murray
+
+Distributed under GNU GPL, version 3.
---
+++ ivy-xref.el
@@ -0,0 +1,64 @@
+;;; ivy-xref.el --- Ivy interface for xref results -*- lexical-binding: t -*-
+
+;; Copyright (C) 2017  Alex Murray <murray.alex@gmail.com>
+
+;; Author: Alex Murray <murray.alex@gmail.com>
+;; URL: https://github.com/alexmurray/ivy-xref
+;; Version: 0.1
+;; Package-Requires: ((emacs "25.1") (ivy "0.10.0"))
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This packages provides ivy as the interface for selection from xref results.
+
+;;;; Setup
+
+;; (require 'ivy-xref)
+;; (setq xref-show-xrefs-function 'ivy-xref-show-xrefs)
+
+;;; Code:
+(require 'xref)
+(require 'ivy)
+
+(defun ivy-xref-make-collection (xrefs)
+  "Transform XREFS into a collection for display via `ivy-read'."
+  (let ((collection nil))
+    (dolist (xref xrefs)
+      (with-slots (summary location) xref
+        (let ((line (xref-location-line location))
+              (file (xref-location-group location))
+              (candidate nil))
+          (setq candidate (concat
+                           ;; use file name only
+                           (car (reverse (split-string file "\\/")))
+                                  (when (string= "integer" (type-of line))
+                                    (concat ":" (int-to-string line) ": "))
+                                  summary))
+          (push `(,candidate . ,location) collection))))
+    collection))
+
+(defun ivy-xref-show-xrefs (xrefs _alist)
+  "Show the list of XREFS via ivy."
+  (ivy-read "xref: " (ivy-xref-make-collection xrefs)
+            :require-match t
+            :sort t
+            :action #'(lambda (candidate)
+                        (xref--show-location (cdr candidate) t))))
+
+(provide 'ivy-xref)
+;;; ivy-xref.el ends here
